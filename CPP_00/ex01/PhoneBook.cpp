54c71577f353a9bed2882508efc60c86db1e6a77
Loic Dubouloz <lulutalu@gmail.com>
2022-09-21 16:45:55 +0200

    Finished CPP_00/ex01

--- CPP_00/ex01/PhoneBook.cpp
+++ CPP_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: lduboulo <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 12:18:46 by lduboulo          #+#    #+#             */
-/*   Updated: 2022/09/21 15:39:19 by lduboulo         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:40:24 by lduboulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,64 @@ void	phoneBook::add_contact(int i)
 	std::cout << "---------------------------------------------" << std::endl;
 }
 
+void	phoneBook::printPhoneBook(void)
+{
+	int			n = 0;
+
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|                 Registry                  |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|  Index   |First Name| Last Name| Nickname |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	while (n < 8)
+	{
+		if (Book[n].getFirstName().empty())
+			break ;
+		std::cout << "|         ";
+		std::cout << n + 1;
+		std::cout << "|";
+		std::cout << stringFormat(Book[n].getFirstName());
+		std::cout << "|";
+		std::cout << stringFormat(Book[n].getLastName());
+		std::cout << "|";
+		std::cout << stringFormat(Book[n].getNickname());
+		std::cout << "|" << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
+		n++;
+	}
+}
+
+void	phoneBook::printContactInformation(int n)
+{
+	if (Book[n - 1].getFirstName().empty())
+	{
+		std::cout << "---------------------------------------------" << std::endl;
+		std::cout << "|             Incorrect entry               |" << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
+	}
+	else
+	{
+		std::cout << "---------------------------------------------" << std::endl;
+		std::cout << "|             Contact number ";
+		std::cout << n;
+		std::cout << "              |" << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
+		std::cout << "| First Name : ";
+		std::cout << Book[n - 1].getFirstName() << std::endl;
+		std::cout << "| Last Name : ";
+		std::cout << Book[n - 1].getLastName() << std::endl;
+		std::cout << "| Nickname : ";
+		std::cout << Book[n - 1].getNickname() << std::endl;
+		std::cout << "| Phone Number : ";
+		std::cout << Book[n - 1].getPhoneNumber() << std::endl;
+		std::cout << "| Darkest Secret : ";
+		std:: cout << Book[n - 1].getDarkestSecret() << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
+	}
+}
+
+/*String manipulation to align the text to the right in the PhoneBook registry*/
+/*Also resize the string if it exceeds 10 characters*/
 std::string	phoneBook::stringFormat(std::string str)
 {
 	if (str.length() >= 10)
@@ -63,94 +121,38 @@ std::string	phoneBook::stringFormat(std::string str)
 	return (str);
 }
 
-void	phoneBook::printPhoneBook(void)
-{
-	int			n = 0;
-	std::string	str;
-
-	std::cout << "---------------------------------------------" << std::endl;
-	std::cout << "|  Index   |First Name| Last Name| Nickname |" << std::endl;
-	std::cout << "---------------------------------------------" << std::endl;
-	while (n < 8)
-	{
-		if (Book[n].getFirstName().empty())
-			break ;
-		std::cout << "|         ";
-		std::cout << n + 1;
-		std::cout << "|";
-		str = Book[n].getFirstName();
-		std::cout << stringFormat(str);
-		std::cout << "|";
-		str = Book[n].getLastName();
-		std::cout << stringFormat(str);
-		std::cout << "|";
-		str = Book[n].getNickname();
-		std::cout << stringFormat(str);
-		std::cout << "|" << std::endl;
-		n++;
-		std::cout << "---------------------------------------------" << std::endl;
-	}
-}
-
-void	phoneBook::printContactInformation(int n)
-{
-	std::string	str;
-
-	str = Book[n - 1].getFirstName();
-	if (str.empty())
-		std::cout << "Can't find this contact\n" << std::endl;
-	else
-	{
-		std::cout << "---------------------------------------------" << std::endl;
-		std::cout << "|             Contact number ";
-		std::cout << n;
-		std::cout << "              |" << std::endl;
-		std::cout << "---------------------------------------------" << std::endl;
-		std::cout << "| First Name : ";
-		std::cout << str << std::endl;
-		std::cout << "| Last Name : ";
-		str = Book[n - 1].getLastName();
-		std::cout << str << std::endl;
-		std::cout << "| Nickname : ";
-		str = Book[n - 1].getNickname();
-		std::cout << str << std::endl;
-		std::cout << "| Phone Number : ";
-		str = Book[n - 1].getPhoneNumber();
-		std::cout << str << std::endl;
-		std::cout << "| Darkest Secret : ";
-		str = Book[n - 1].getDarkestSecret();
-		std:: cout << str << std::endl;
-		std::cout << "---------------------------------------------" << std::endl;
-	}
-}
-
+/* Brain of the SEARCH action*/
 void	phoneBook::search_contact(void)
 {
 	std::string	str;
 	int			n;
 
 	if (getContactNbr() == 0 && Book[0].getFirstName().empty())
-		std::cout << "Please add a contact first" << std::endl;
+	{
+		std::cout << "---------------------------------------------" << std::endl;
+		std::cout << "|        Please add a contact first         |" << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
+	}
 	else
 	{
 		printPhoneBook();
 		str.clear();
-		std::cout << "Please insert the index number of the contact : ";
+		std::cout << "Contact's index : ";
 		while (str.length() == 0)
 			std::getline(std::cin, str);
 		std::stringstream(str) >> n;
 		if (n >= 1 && n <= 8)
 			printContactInformation(n);
 		else
-			std::cout << "Can't find this contact" << std::endl;
+		{
+			std::cout << "---------------------------------------------" << std::endl;
+			std::cout << "|             Incorrect entry               |" << std::endl;
+			std::cout << "---------------------------------------------" << std::endl;
+		}
 	}
 }
 
-int		phoneBook::getContactNbr(void)
-{
-	return (i);
-}
-
+/*Quick manipulation of counter 'i' once it exceeds the 8 contact limit*/
 void	phoneBook::contact_n(void)
 {
 	i++;
@@ -158,6 +160,12 @@ void	phoneBook::contact_n(void)
 		i = 0;
 }
 
+int		phoneBook::getContactNbr(void)
+{
+	return (i);
+}
+
+/* Constructor. Used to initilaize (int i) and to print a welcome Message*/
 phoneBook::phoneBook(void)
 {
 	i = 0;
